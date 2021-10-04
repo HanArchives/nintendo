ad45f223f943acde57d7c5de8b53043e5f1e6688
HanOssef <90187643+HanOssef@users.noreply.github.com>
2021-10-04 17:13:20 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -6,3 +6,18 @@ deButton.addEventListener("click", toggleMenu);
 function toggleMenu() {
     hetMenu.classList.toggle('toonMenu');
 }
+
+
+var hamburgerBtn = document.querySelector('.hamburger_menu');
+var menuOpen = false;
+hamburgerBtn.addEventListener('click', function () {
+
+    // if(! MenuOpen) lees je als if( MenuOpen “is niet waar”)
+    if (!menuOpen) {
+        hamburgerBtn.classList.add('openMenu');
+        menuOpen = true;
+    } else {
+        hamburgerBtn.classList.remove('openMenu');
+        menuOpen = false;
+    }
+});
